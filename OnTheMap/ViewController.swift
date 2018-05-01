921382b747080d4836e8e05f408f8d1d632afba8
Sonalboraste <sonalboraste@gmail.com>
2018-05-01 07:22:32 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Sonal on 5/1/18.
+//  Copyright © 2018 Sonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
